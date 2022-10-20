5c41c074ef9a55ca1ca17aa7d35b512450cda85f
cosmic <kingsleyfrancis42@gmail.com>
2022-10-20 15:54:25 +0100

    emmetill

---
+++ mkdir.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "syscall.h"
+#include "dirent.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <ctype.h>
+#include <unistd.h>
+int main(int argc , char *argv[]){
+	printf("%s\n",wd);
+	free(wd);
+	return (0);
+}
